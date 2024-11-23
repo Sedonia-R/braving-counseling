f3dc00caac35d6e9535f5cdaebd2f9b90fd4d7a1
Sedonia <river.sedonia@gmail.com>
2024-11-22 22:10:34 -0500

    created MobileSwiper component to allow user swipe when on touch screen device

---
+++ src/components/MobileSwiper.js
@@ -0,0 +1,46 @@
+import { useCallback, useEffect, useState, useRef } from "react";
+
+export default function MobileSwiper({ children, onSwipe}) {
+  const wrapperRef = useRef(null);
+  const [startX, setStartX] = useState(0);
+  const [startY, setStartY] = useState(0);
+
+  const handleTouchStart = useCallback((e) => {
+    if (!wrapperRef.current.contains(e.target)) {
+      return;
+    }
+
+    e.preventDefault();
+
+    setStartX(e.touches[0].clientX);
+    setStartY(e.touches[0].clientY);
+  }, []);
+
+  const handleTouchEnd = useCallback((e) => {
+    if (!wrapperRef.current.contains(e.target)) {
+      return;
+    }
+
+    e.preventDefault();
+
+    const endX = e.changedTouches[0].clientX;
+    const endY = e.cahngedTouches[0].clientY;
+    const deltaX = endX - startX;
+    const deltaY = endY - startY;
+
+    onSwipe({ deltaX, deltaY })
+  }, [startX, startY, onSwipe]);
+
+
+  useEffect(() => {
+    window.addEventListener("touchstart", handleTouchStart);
+    window.addEventListener("touchend", handleTouchEnd);
+
+    return() => {
+      window.removeEventListener("touchstart, handleTouchStart");
+      window.removeEventListener("touchend", handleTouchEnd);
+    }
+  }, [handleTouchStart, handleTouchEnd])
+
+  return <div ref={wrapperRef}>{children}</div>;
+}
